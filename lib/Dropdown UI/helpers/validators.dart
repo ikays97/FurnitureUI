8589e6fcc795669608f40e0652c64b547d9fcaa1
Atabek <44347973+ikays97@users.noreply.github.com>
2022-05-03 15:38:20 +0500

    worked on 'login sheet'

---
+++ lib/Dropdown UI/helpers/validators.dart
@@ -0,0 +1,13 @@
+// TODO: email validator (RegEx)
+
+// TODO: phone validator (RegEx)
+// - max 6 character
+String? phoneValidator(String? value) {
+  if (value == null || value.isEmpty) {
+    return "Doldyrow shuyeri";
+  } else {
+    return null;
+  }
+}
+
+// 
--- lib/Dropdown UI/widgets/bottom_login_sheet.dart
+++ lib/Dropdown UI/widgets/bottom_login_sheet.dart
@@ -1,11 +1,12 @@
-import 'dart:ui';
-
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../helpers/validators.dart';
 
 class BottomLoginSheet extends StatelessWidget {
   const BottomLoginSheet({Key? key}) : super(key: key);
 
-  void _close(BuildContext context){
+  void _close(BuildContext context) {
     if (Navigator.canPop(context)) {
       Navigator.pop(context);
     }
@@ -17,36 +18,40 @@ class BottomLoginSheet extends StatelessWidget {
       child: Container(
         width: double.infinity,
         color: Colors.white,
-        padding: const EdgeInsets.only(top:10, bottom: 50,left: 30, right: 30),
+        padding: EdgeInsets.only(
+          top: 30,
+          bottom: 30 + MediaQuery.of(context).viewInsets.bottom,
+          left: 30,
+          right: 30,
+        ),
         child: Column(
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children:[
-              Container(
-                padding: const EdgeInsets.all(8),
-                decoration: const BoxDecoration(
-                color: Colors.deepOrange,
-                  shape: BoxShape.circle
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: const BoxDecoration(
+                      color: Colors.deepOrange, shape: BoxShape.circle),
+                  child: const Text(
+                    'YES.',
+                    style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: -1.2),
+                  ),
                 ),
-                child: const Text(
-                  'YES.',
-                  style: TextStyle(fontSize: 15, color: Colors.white, fontWeight: FontWeight.w500, letterSpacing: -1.2),
+                GestureDetector(
+                  onTap: () {
+                    _close(context);
+                  },
+                  child: Container(child: Icon(Icons.close)),
                 )
-              ),
-              GestureDetector(
-                onTap: (){ _close(context);},
-                child: Container(
-                  child: Icon(Icons.close)
-                ),
-              )
-    
-              ]
+              ],
             ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 20),
-              child: _BootmSheetLogin(),
-            )
+            SizedBox(height: 20),
+            _BootmSheetLogin(),
           ],
         ),
       ),
@@ -62,144 +67,212 @@ class _BootmSheetLogin extends StatefulWidget {
 }
 
 class _BootmSheetLoginState extends State<_BootmSheetLogin> {
-  bool _isTapped = false;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  void _onTap(){
-     setState(()  {
-      _isTapped = !_isTapped;
-      _changeBg();
-    });
+  final phoneNumberController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
   }
 
-  Future<void> _changeBg()async{
-      await Future.delayed(const Duration(milliseconds: 300));
-      setState(() {
-      _isTapped = false;
-        
-      });
+  @override
+  void dispose() {
+    phoneNumberController.dispose();
+    super.dispose();
   }
+
+  get _phoneFieldPrefixIcon {
+    return Container(
+      width: 64,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(width: 12),
+          Text(
+            '+993',
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+          SizedBox(width: 5),
+          SizedBox(
+            height: 48,
+            child: VerticalDivider(
+              indent: 10,
+              endIndent: 10,
+              width: 3,
+              thickness: 0.6,
+              color: Colors.black26,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Form(
+      key: _formKey,
+      child: Container(
         width: double.infinity,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children:[
+          children: [
             Container(
               width: double.infinity,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children:  [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical:10.0),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children:  [
-                        Text('Login', style: LoginStyleUtils().blueTextBold,),
-                          Padding(
-                          padding: const EdgeInsets.symmetric(horizontal:8.0),
-                          child: Text('or', style: LoginStyleUtils().orText),
-                        ),
-                        Text('Signup', style: LoginStyleUtils().blueTextBold,),
-                      ],
-                    ),
-                  ),
-                   Container(
-                     padding: const EdgeInsets.symmetric(vertical: 15),
-                    width: double.infinity,
-                    height: 70,
-                    child: TextField(
-                      keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          label: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Stack(
-                              clipBehavior: Clip.none,
-                              children:  [
-                                 Text('Mobile Number', style: LoginStyleUtils().labelText),
-                                Positioned(
-                                  top: 0,
-                                  right:-8,
-                                  child: Text('*', style: LoginStyleUtils().pinkText)),
-                              ],
+                children: [
+                  SizedBox(height: 10),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                          text: 'Login',
+                          style: LoginStyleUtils().blueTextBold,
+                          children: [
+                            TextSpan(
+                                text: '  or  ',
+                                style: Theme.of(context).textTheme.bodyText1),
+                            TextSpan(
+                              text: 'Signup',
                             ),
-                          ),
-                          focusedBorder: const OutlineInputBorder(borderSide: BorderSide(color: Colors.black38)),
-                          contentPadding: const EdgeInsets.symmetric(vertical: 5),
-                          prefixIcon: Container(
-                            width: 50,
-                            child: Padding(
-                              padding: const EdgeInsets.all(5.0),
-                              child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children:  [
-                                    const Text('+993', style: TextStyle(color: Colors.black54),), 
-                                    SizedBox(width: 5,), 
-                                    Container(width: 1, height: 15, color: Colors.black26,)],),
-                            )),
-                            enabledBorder:const OutlineInputBorder(borderSide: BorderSide(color: Colors.black12)) ,
+                          ],
                         ),
-                    ),
-                    ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical:10.0),
-                    child: Row(
-                      children:  [
-                        const Text(
-                          'By continuing, I agree to the',
-                        ),
-                        Text(
-                          ' Terms of Use',
-                          style: LoginStyleUtils().pinkTextBold,
-                        ),
-                        const Text(
-                          ' &',
-                        ),
-                        Text(
-                          ' Privacy Piolicy',
-                          style: LoginStyleUtils().pinkTextBold,
-                        ),
-                      ],
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: _onTap,
-                    child: Container(
-                      margin: const EdgeInsets.only(top:20),
-                      decoration: BoxDecoration(
-                      color: _isTapped?  Color.fromARGB(255, 247, 133, 171) :Colors.pinkAccent,
-
-                        borderRadius: BorderRadius.circular(3)
                       ),
-                      padding: const EdgeInsets.symmetric(vertical: 7),
-                      alignment: Alignment.center,
-                      child:  Text('CONTINUE' ,style: LoginStyleUtils().btnText),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  Container(
+                    width: double.infinity,
+                    child: TextFormField(
+                      controller: phoneNumberController,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        label: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Stack(
+                            clipBehavior: Clip.none,
+                            children: [
+                              Text('Mobile Number',
+                                  style: LoginStyleUtils().labelText),
+                              Positioned(
+                                top: 0,
+                                right: -10,
+                                child: Text('*',
+                                    style: LoginStyleUtils().pinkText),
+                              ),
+                            ],
+                          ),
+                        ),
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black12),
+                        ),
+                        focusedBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                          color: Colors.black38,
+                        )),
+                        errorBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                          color: Colors.pinkAccent,
+                        )),
+                        focusedErrorBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                          color: Colors.pinkAccent,
+                        )),
+                        contentPadding: const EdgeInsets.symmetric(vertical: 5),
+                        prefixIcon: this._phoneFieldPrefixIcon,
+                      ),
+                      validator: phoneValidator,
+                      inputFormatters: [
+                        LengthLimitingTextInputFormatter(6),
+                      ],
                     ),
                   ),
-                  Container(
-                    margin: const EdgeInsets.only(top: 35),
-                     padding:  const EdgeInsets.only(top:8.0),
-                    child: Row(
-                      children:  [
-                        const Text('Having trouble logging in?'),
-                        Text(' Get help', style: LoginStyleUtils().pinkText,),
+                  SizedBox(height: 16),
+                  RichText(
+                    text: TextSpan(
+                      text: 'By continuing, I agree to the',
+                      children: [
+                        TextSpan(
+                          text: ' Terms of Use',
+                          style: LoginStyleUtils().pinkTextBold,
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              print("entered");
+                            },
+                        ),
+                        TextSpan(text: ' &'),
+                        TextSpan(
+                          text: ' Privacy Piolicy',
+                          style: LoginStyleUtils().pinkTextBold,
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              print("entered");
+                            },
+                        ),
                       ],
+                    ),
+                  ),
+                  SizedBox(height: 24),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: _onTap,
+                      child: Text('CONTINUE', style: LoginStyleUtils().btnText),
+                      style:
+                          ElevatedButton.styleFrom(primary: Colors.pinkAccent),
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  Align(
+                    alignment: Alignment.center,
+                    child: RichText(
+                      text: TextSpan(
+                        text: 'Having trouble logging in? ',
+                        style: Theme.of(context).textTheme.bodyText2,
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'Get help',
+                            style: LoginStyleUtils().pinkText,
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                print("entered");
+                              },
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ],
               ),
             ),
-          ]
+          ],
         ),
-      );
+      ),
+    );
+  }
+
+  void _onTap() {
+    if (_formKey.currentState?.validate() == true) {
+      // logic
+    }
   }
 }
 
-class LoginStyleUtils{
-   TextStyle pinkTextBold = const TextStyle(color: Colors.pinkAccent, fontWeight: FontWeight.bold);
-   TextStyle blueTextBold = const TextStyle( color: Color.fromARGB(255, 6, 14, 61), fontSize: 20, fontWeight: FontWeight.bold);
-   TextStyle pinkText = const TextStyle(color: Colors.pinkAccent,);
-   TextStyle labelText = const TextStyle(letterSpacing: .15,fontSize: 13, color: Colors.black45);
-   TextStyle btnText = const TextStyle(fontSize: 15, color: Colors.white, fontWeight: FontWeight.w500);
-   TextStyle orText = const TextStyle(fontSize: 14,  color: Color.fromARGB(255, 6, 14, 61),);
+class LoginStyleUtils {
+  TextStyle pinkTextBold =
+      const TextStyle(color: Colors.pinkAccent, fontWeight: FontWeight.bold);
+  TextStyle blueTextBold = const TextStyle(
+      color: Color.fromARGB(255, 6, 14, 61),
+      fontSize: 20,
+      fontWeight: FontWeight.bold);
+  TextStyle pinkText = const TextStyle(
+    color: Colors.pinkAccent,
+  );
+  TextStyle labelText =
+      const TextStyle(letterSpacing: .15, fontSize: 13, color: Colors.black45);
+  TextStyle btnText = const TextStyle(
+      fontSize: 15, color: Colors.white, fontWeight: FontWeight.w500);
 }
